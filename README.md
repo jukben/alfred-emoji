fef78bcd13f1b4ca3a4c0f7099a4db398dea0de0
Jakub Benes <jukben@gmail.com>
2018-02-20 00:18:05 +0100

    feat: added offline mode

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ search for you favorite emoji without struggle. 🤓
 <img src="https://i.imgur.com/DZeggu3.png" width="700"></div>
 Screen is from version 0.0.3. See version [0.0.2](https://i.imgur.com/OL3WST0.png)
 
+# Usage
+
+With this workflow, you can search for you emoji with an ease. Just type **e**. If there is no connection available will fallback to offline vocabulary. You can enforce this behavior in settings. 
+
 ## Install
 
 `npm i -g alfred-emoji`  
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const alfy = require('alfy');
-const search = require('@jukben/emoji-search');
-const unicode = require('@jukben/unicode').default;
-const emojis = require('emoji-datasource-apple');
+const path = require("path");
+const alfy = require("alfy");
+const search = require("@jukben/emoji-search");
+const unicode = require("@jukben/unicode").default;
+const emojis = require("emoji-datasource-apple");
+
+const ALWAYS_OFFLINE = +process.env["always-offline"] || 0;
 
 const emojiLookup = emojis.reduce((a, v, i, arr) => {
   a[v.unified.toLowerCase()] = v;
@@ -14,41 +16,61 @@ const emojiLookup = emojis.reduce((a, v, i, arr) => {
 const getHex = char => unicode(char).hexCodeAt(0);
 
 function getImagePath(char) {
-  return `${path.dirname(
-    require.resolve('emoji-datasource-apple')
-  )}/img/apple/64/${getHex(char)}.png`;
+  return `${path.dirname(require.resolve("emoji-datasource-apple"))}/img/apple/64/${getHex(char)}.png`;
 }
 
 function getName(char) {
-  return emojiLookup[getHex(char)].name || char;
+  return (emojiLookup[getHex(char)] && emojiLookup[getHex(char)].name) || char;
 }
 
-const emoji = () =>
+const emoji = () => {
+  const format = char => {
+    return {
+      title: "",
+      subtitle: getName(char).toLowerCase(),
+      icon: { path: getImagePath(char) },
+      arg: char
+    };
+  };
+
+  const nothingFound = [{ title: "Nothing found!", icon: { path: getImagePath("👀") } }];
+
+  const getOfflineEmoji = () => {
+    let results = search(alfy.input)
+      .map(({ char }) => char)
+      .filter(char => char)
+      .map(format)
+
+      if (!results.length) {
+        results = nothingFound; 
+      }
+
+      return results;
+  }
+
+  if (ALWAYS_OFFLINE) {
+    return alfy.output(getOfflineEmoji());
+  }
+
   alfy
-    .fetch('emoji.getdango.com/api/emoji', {
+    .fetch("emoji.getdango.com/api/emoji", {
+      maxAge: 8640000000, // cache for 24 hours
       query: {
         q: alfy.input,
-        syn: 1,
-      },
+        syn: 1
+      }
     })
     .then(({ results, synonyms = [] } = {}) => {
       if (!results) {
-        return alfy.output([
-          { title: 'Nothing found!', icon: { path: getImagePath('👀') } },
-        ]);
+        return alfy.output(nothingFound);
       }
 
-      return alfy.output(
-        [
-          ...new Set([...synonyms, ...results.map(({ text }) => text)]),
-        ].map(char => ({
-          title: '',
-          subtitle: getName(char).toLowerCase(),
-          icon: { path: getImagePath(char) },
-          arg: char,
-        }))
-      );
+      return alfy.output([...new Set([...synonyms, ...results.map(({ text }) => text)])].map(format));
+    })
+    .catch(() => {
+      alfy.output(getOfflineEmoji())
     });
+};
 
 emoji();
 
